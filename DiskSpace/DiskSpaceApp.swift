71550e318a678b573287be8a62275f8f1fa3cd70
StuFF mc <m+git@carrascomolina.com>
2021-11-23 12:47:31 +0100

    Initial Commit

---
+++ DiskSpace/DiskSpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiskSpaceApp.swift
+//  DiskSpace
+//
+//  Created by StuFF mc on 23.11.21.
+//
+
+import SwiftUI
+
+@main
+struct DiskSpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
